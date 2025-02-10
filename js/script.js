66506cbcd8798c121117d91a57e0c9aabd947757
Barakaev09 <xumoyunbarakayev2009@gmail.com>
2025-02-10 15:47:00 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -7,3 +7,10 @@ btn.addEventListener('click', (e) => {
     menu.classList.toggle('header_menu_active');
     btn.classList.toggle('header_burger_active');
 });
+
+// document.body.addEventListener('click', (e) => {
+//     if (!e.target.closest('.header_menu')) {
+//         menu.classList.remove('header_menu_active');
+//         btn.classList.remove('header_burger_active');
+//     }
+// });
